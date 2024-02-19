fc5c3b969126d5b4680772491d668fe9d5022429
Mati Gomez <magomezdev@gmail.com>
2024-02-19 09:53:15 -0300

    tabs bg-color and simple :hover

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -6,16 +6,20 @@ export const Tabs = () => {
   return (
     <Tab.Group >
       <Tab.List
-      as="nav"
-      className="mx-auto max-w-7xl bg-zinc-300 pt-8 px-8 text-pretty ">
-        <Tab className="border-none font-bold text-xl px-4 focus:outline-none focus:ring-0 focus:bg-[#07876a] focus:bg-opacity-60 rounded-lg">Home</Tab>
-        <Tab className="border-none font-bold text-xl px-4 focus:outline-none focus:ring-0 focus:bg-[#07876a] focus:bg-opacity-60 rounded-lg">CV</Tab>
-        
+        as="nav"
+        className="mx-auto max-w-7xl bg-zinc-300 pt-8 px-8 text-pretty ">
+        <Tab className="border-none font-bold text-xl px-4 active:bg-[#07876a] active:bg-opacity-90 focus:outline-none focus:ring-0 focus:bg-[#07876a] focus:bg-opacity-60 hover:bg-[#07876a] hover:bg-opacity-30 rounded-lg">
+          Home
+          </Tab>
+        <Tab className="border-none font-bold text-xl px-4 active:bg-[#07876a] active:bg-opacity-10 focus:outline-none focus:ring-0 focus:bg-[#07876a] focus:bg-opacity-60 hover:bg-[#07876a] hover:bg-opacity-30 rounded-lg">
+          CV
+          </Tab>
+
       </Tab.List>
       <Tab.Panels>
-        <Tab.Panel><Landing/></Tab.Panel>
+        <Tab.Panel><Landing /></Tab.Panel>
         <Tab.Panel><CurriculumVitae /></Tab.Panel>
-        
+
       </Tab.Panels>
     </Tab.Group>
   )

996b9023a0a1914d3c746d3ea82b5cfabbd19371
Mati Gomez <magomezdev@gmail.com>
2024-04-29 17:15:12 -0300

    db testing set up

--- src/components/torre/DiceRoll.jsx
+++ src/components/torre/DiceRoll.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react"
 
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/cuentos');
+  const res = await fetch('http://magomezdev.tech/api/cuentos');
 
   if (!res.ok) {
     throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);

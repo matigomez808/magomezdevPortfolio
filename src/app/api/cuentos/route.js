1e2e5b0cca9be1c4662f584a065aff307ba51fe5
Mati Gomez <magomezdev@gmail.com>
2024-04-29 17:36:43 -0300

    dbTesting

--- src/app/api/cuentos/route.js
+++ src/app/api/cuentos/route.js
@@ -13,7 +13,7 @@ export const GET = async (request) => {
       "Access-Control-Allow-Headers": "Content-Type", // Allow Content-Type header
     };
     
-    return new NextResponse(JSON.stringify(cuentos), {status:200})
+    return new NextResponse(JSON.stringify(cuentos), {status:200, headers})
   } catch(error) {
     return new NextResponse('Error in fetching cuentos' + error, {status:500})
   }
